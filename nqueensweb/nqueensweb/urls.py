8966398cf0461c3a1d6f0d0d16b05dd395d99f7b
Syed Ali Rizwan <alirizwan2003@gmail.com>
2024-06-12 13:56:59 +0500

    commit three

--- nqueensweb/nqueensweb/urls.py
+++ nqueensweb/nqueensweb/urls.py
@@ -24,6 +24,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('nqueens.urls'))
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
