1fe74354f0e80269ef2c3ea5cb4afa32e05ded11
Syed Ali Rizwan <alirizwan2003@gmail.com>
2024-06-12 13:46:57 +0500

    fixed deployment requirements

--- nqueensweb/nqueensweb/settings.py
+++ nqueensweb/nqueensweb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3w$@o$&nw#_p_t=ildt0s)(1gx^f-yflf(6suz1et4kv5#t+hr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -121,7 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
